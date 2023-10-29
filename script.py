84d4f1da192badd61b2dbd8c1fc47596d8bdcc6e
Arthur Conmy <conmy.a@northeastern.edu>
2023-10-29 16:57:17 +0000

    Big LR Lambda sweep

--- script.py
+++ script.py
@@ -14,14 +14,14 @@ def run_script(threshold, gpu_id, kwargs): # Note fake kwargs
 
 if __name__ == '__main__':
 
-    num_gpus = 6 # specify the number of GPUs available
+    num_gpus = 1 # specify the number of GPUs available
     num_jobs_per_gpu = 2 # specify the number of jobs per GPU
 
     pool = multiprocessing.Pool(num_gpus * num_jobs_per_gpu)
     jobs = []
 
     for it in range(3, int(1e6)):        
-        curspace = list(product([9e-5, 2e-4, 4e-4], [1e-4, 2e-4, 4e-4, 5e-4]))
+        curspace = list(product([2e-4], (1e-4 * np.linspace(3, 5, 12)).tolist()))
 
         if not isinstance(curspace, list):
             curspace = curspace[1:-1]
