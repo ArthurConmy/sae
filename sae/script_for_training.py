973db0fbb37ecf1f94bf2cda8d6f4d53aabaa686
ArthurConmy <35957051+ArthurConmy@users.noreply.github.com>
2023-10-31 15:56:52 +0000

    Update script_for_training.py

--- sae/script_for_training.py
+++ sae/script_for_training.py
@@ -15,14 +15,14 @@ def run_script(threshold, gpu_id, kwargs): # Note fake kwargs
 
 if __name__ == '__main__':
 
-    num_gpus = 6 # specify the number of GPUs available
-    num_jobs_per_gpu = 3 # specify the number of jobs per GPU
+    num_gpus = 7 # specify the number of GPUs available
+    num_jobs_per_gpu = 1 # specify the number of jobs per GPU
 
     pool = multiprocessing.Pool(num_gpus * num_jobs_per_gpu)
     jobs = []
 
     for it in range(3, int(1e6)):        
-        curspace = list(product([1e-4], (1e-4 * np.linspace(4, 4.6, 9)).tolist(), [69, 420]))
+        curspace = list(product([1e-4, 1.5 * 1e-4, 0.75 * 1e-4], (1e-4 * np.linspace(3.5, 4, 4.5)).tolist(), [1]))
 
         if not isinstance(curspace, list):
             curspace = curspace[1:-1]
