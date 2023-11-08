20791783e20ff7fdc820f93127d518517485f97d
Arthur Conmy <conmy.a@northeastern.edu>
2023-11-08 02:21:40 +0000

    Upd

--- sae/script_for_training.py
+++ sae/script_for_training.py
@@ -25,11 +25,11 @@ if __name__ == '__main__':
     keyword_list = []
 
     for width in [2048, 16384*8, 16384]:
-        for lr in [5.5 * 1e-5, 1e-5]:
+        for lr in [5.5 * 1e-5, 3e-5]:
             for l1_lambda in [0.0012, 0.001]:
-                keyword_list.append({"d_sae": width, "lr": lr})
-                if width > 100_000:
-                    keyword_list[-1]["buffer_size"] = 2**16
+                keyword_list.append({"d_sae": width, "lr": lr, "l1_lambda": l1_lambda})
+                # if width > 100_000:
+                    # keyword_list[-1]["buffer_size"] = 2**16
 
     for threshold_idx, keywords in enumerate(keyword_list):
         gpu_id = (threshold_idx // num_jobs_per_gpu) % num_gpus
