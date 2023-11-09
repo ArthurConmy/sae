2599d119d976b49d406ab52c5a90e5bf1f213e03
Arthur Conmy <conmy.a@northeastern.edu>
2023-11-09 02:49:39 +0000

    Push

--- sae/script_for_training.py
+++ sae/script_for_training.py
@@ -18,20 +18,22 @@ def run_script(threshold, gpu_id, keywords):
 
 if __name__ == '__main__':
 
-    num_gpus = 4 # Number of GPUs available
+    num_gpus = 6 # Number of GPUs available
     num_jobs_per_gpu = 1  # Number of jobs per GPU
 
     pool = multiprocessing.Pool(num_gpus * num_jobs_per_gpu)
     jobs = []
     keyword_list = []
 
-    # for width in [16384*8, 16384*4]: # [2048, 16384*8, 16384]:
-    #     for l1_lambda in (torch.FloatTensor([8, 12, 16]) / 10_000).tolist(): # [0.0013, 0.001]:
+    for width in [16384*8, 16384*4]: # [2048, 16384*8, 16384]:
+        for l1_lambda in (torch.FloatTensor([8, 12, 16]) / 10_000).tolist(): # [0.0013, 0.001]:
+            keyword_list.append({"d_sae": width, "lr": 0.0012, "l1_lambda": l1_lambda})
+
+    # for width in [16384, 16384//8]: # [2048, 16384*8, 16384]:
+    #     for l1_lambda in (torch.FloatTensor([8, 16]) / 10_000).tolist(): # [0.0013, 0.001]:
     #         keyword_list.append({"d_sae": width, "lr": 0.0012, "l1_lambda": l1_lambda})
 
-    for width in [16384, 16384//8]: # [2048, 16384*8, 16384]:
-        for l1_lambda in (torch.FloatTensor([8, 16]) / 10_000).tolist(): # [0.0013, 0.001]:
-            keyword_list.append({"d_sae": width, "lr": 0.0012, "l1_lambda": l1_lambda})
+    keyword_list[-1]["delete_cache"]=True
 
     # keyword_list.append(
     #     {
