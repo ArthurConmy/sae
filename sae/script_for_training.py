02c86157c392de0d06f8f06cea80b0a789fdc690
Arthur Conmy <conmy.a@northeastern.edu>
2023-11-09 02:37:55 +0000

    end

--- sae/script_for_training.py
+++ sae/script_for_training.py
@@ -18,17 +18,16 @@ def run_script(threshold, gpu_id, keywords):
 
 if __name__ == '__main__':
 
-    num_gpus = 5 # Number of GPUs available
-    num_jobs_per_gpu = 2  # Number of jobs per GPU
+    num_gpus = 6 # Number of GPUs available
+    num_jobs_per_gpu = 1  # Number of jobs per GPU
 
     pool = multiprocessing.Pool(num_gpus * num_jobs_per_gpu)
     jobs = []
     keyword_list = []
 
-    # for width in [16384*8]: # [2048, 16384*8, 16384]:
-    #     for lr in [8e-4, 12 * 1e-4]:
-    #         for l1_lambda in (torch.FloatTensor([8, 12]) / 10_000).tolist(): # [0.0013, 0.001]:
-    #             keyword_list.append({"d_sae": width, "lr": lr, "l1_lambda": l1_lambda})
+    for width in [16384*8, 16384*4]: # [2048, 16384*8, 16384]:
+        for l1_lambda in (torch.FloatTensor([8, 12, 16]) / 10_000).tolist(): # [0.0013, 0.001]:
+            keyword_list.append({"d_sae": width, "lr": lr, "l1_lambda": l1_lambda})
 
     # keyword_list.append(
     #     {
@@ -38,15 +37,18 @@ if __name__ == '__main__':
     #     },
     # )
 
-    keyword_list = [
-        {"d_sae": 16384, "lr": 8e-4, "l1_lambda": 12 / 10_000},
-        {"d_sae": 16384, "lr": 12 * 1e-4, "l1_lambda": 12 / 10_000}, 
-        {"d_sae": 16384*16, "lr": 8e-4, "l1_lambda": 12 / 10_000},
-        {"d_sae": 16384*16, "lr": 12*1e-4, "l1_lambda": 12 / 10_000}, # Two super promising runs
-        {"d_sae": 16384*16, "lr": 12*1e-4, "l1_lambda": 16 / 10_000}, # Two super promising runs - another in a similar vein, scaling like Anthropic did
-        {"d_sae": 16384, "lr": 8 * 1e-4, "l1_lambda": 8 / 10_000}, # Also stick these on GPU 0-1
-        {"d_sae": 16384, "lr": 12 * 1e-4, "l1_lambda": 8 / 10_000},
-    ]
+    # keyword_list = [
+    #     {},
+    #     {},
+    #     {} 
+        # {"d_sae": 16384, "lr": 8e-4, "l1_lambda": 12 / 10_000},
+        # {"d_sae": 16384, "lr": 12 * 1e-4, "l1_lambda": 12 / 10_000}, 
+        # {"d_sae": 16384*16, "lr": 8e-4, "l1_lambda": 12 / 10_000},
+        # {"d_sae": 16384*16, "lr": 12*1e-4, "l1_lambda": 12 / 10_000}, # Two super promising runs
+        # {"d_sae": 16384*16, "lr": 12*1e-4, "l1_lambda": 16 / 10_000}, # Two super promising runs - another in a similar vein, scaling like Anthropic did
+        # {"d_sae": 16384, "lr": 8 * 1e-4, "l1_lambda": 8 / 10_000}, # Also stick these on GPU 0-1
+        # {"d_sae": 16384, "lr": 12 * 1e-4, "l1_lambda": 8 / 10_000},
+    # ]
 
 # if width > 100_000:
     # keyword_list[-1]["buffer_size"] = 2**16
