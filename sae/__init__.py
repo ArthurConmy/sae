8bff8f6ecd51a01428dfb90296c279d84108559d
ArthurConmy <35957051+ArthurConmy@users.noreply.github.com>
2023-11-09 14:42:27 +0000

    Update __init__.py

--- sae/__init__.py
+++ sae/__init__.py
@@ -6,3 +6,6 @@ torch.set_float32_matmul_precision('high') # When training if float32, torch.com
 import einops
 from einops._torch_specific import allow_ops_in_compiled_graph # requires einops>=0.6.1
 allow_ops_in_compiled_graph()
+
+from .model import * 
+from .utils import *
